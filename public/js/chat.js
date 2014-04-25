893c249f0b1842671f1a68fb0ff16fbe64655f82
jacobedawson <reviber8@gmail.com>
2014-04-25 17:22:06 +0200

    Adjust scroll issue (no. 50)
    Fixed more scroll issues based upon div ids

--- public/js/chat.js
+++ public/js/chat.js
@@ -143,8 +143,11 @@
 					tabNotify(data);	
 					$chat.append('<div class="msg"><div class="msgFrom"><b>' + data.nick + ': </b></div>' + 
 						'<div class ="msgBody">' + data.msg + '</div>' + "</div><br/>");
-					$chat.scrollTop($chat[0].scrollHeight);
-
+					if(data.to == null) {
+						$chat.scrollTop($chat[0].scrollHeight);
+					} else {
+						$chat.scrollTop($('#msgBox' + data.to)[0].scrollHeight);
+					}
 			}
 
 			//private messaging 
